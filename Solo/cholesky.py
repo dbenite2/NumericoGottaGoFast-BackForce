6cc031e37623ca387af16864898fe49dcc49d4e3
jalzat39 <43329623+jalzat39@users.noreply.github.com>
2019-05-11 23:46:36 -0500

    para n en cholesky

--- Solo/cholesky.py
+++ Solo/cholesky.py
@@ -10,23 +10,14 @@ def cholesky(A):
     for i, (Ai, Li) in enumerate(zip(A, L)):
         for j, Lj in enumerate(L[:i+1]):
             s = sum(Li[k] * Lj[k] for k in range(j))
-            Li[j] = sqrt(Ai[i] - s) if (i == j) else \
-                      (1.0 / Lj[j] * (Ai[j] - s))
+            Li[j] = sqrt(Ai[i] - s) if (i == j) else (1.0 / Lj[j] * (Ai[j] - s))
     return L
  
-a11 = float(input('Ingresa el valor a11: '))
-a12 = float(input('Ingresa el valor a12: '))
-a13 = float(input('Ingresa el valor a13: '))
-a21 = float(input('Ingresa el valor a21: '))
-a22 = float(input('Ingresa el valor a22: '))
-a23 = float(input('Ingresa el valor a23: '))
-a31 = float(input('Ingresa el valor a31: '))
-a32 = float(input('Ingresa el valor a32: '))
-a33 = float(input('Ingresa el valor a33: '))
+rows = int(input("Ingresa el tamaño de la matriz: "))
+print("Ingresa la matriz %s x %s: "% (rows, rows))
+for i in range(rows):
+    tablaA.append(list(map(int, input().rstrip().split())))
 
-tablaA.append([a11,a12,a13])
-tablaA.append([a21,a22,a23])
-tablaA.append([a31,a32,a33])
 L = cholesky(tablaA)
 
 print("A:")

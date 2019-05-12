0af007d777a9d1dc94ef81f020cc15ab9d1533c1
jalzat39 <43329623+jalzat39@users.noreply.github.com>
2019-05-12 14:57:28 -0500

    corregido el error de cholesky

--- Solo/cholesky.py
+++ Solo/cholesky.py
@@ -4,17 +4,33 @@ from math import sqrt
 from pprint import pprint
 
 tablaA = []
+ 
+def cholesky(C):
+    
+    m = len(C)
 
-def cholesky(A):
-    L = [[0.0] * len(A) for _ in range(len(A))]
-    for i, (Ai, Li) in enumerate(zip(A, L)):
-        for j, Lj in enumerate(L[:i+1]):
-            s = sum(Li[k] * Lj[k] for k in range(j))
-            Li[j] = sqrt(Ai[i] - s) if (i == j) else (1.0 / Lj[j] * (Ai[j] - s))
+    L = [[0.0] * m for i in range(m)]
+
+    for i in range(m):
+        for j in range(i+1):
+            Temp = sum(L[i][k] * L[j][k] for k in range(j))
+            
+		# For Diagonal elements
+            if (i == j):
+                
+                L[i][j] = sqrt(C[i][i] - Temp)
+            else:
+                # For Non Diagonal Elements
+                L[i][j] = (1.0 / L[j][j] * (C[i][j] - Temp))
     return L
  
-rows = int(input("Ingresa el tamaño de la matriz: "))
-print("Ingresa la matriz %s x %s: "% (rows, rows))
+rows = int(input("Ingresa el tamaño de la fila: "))
+colum = int(input("Ingresa el tamaño de la columna: "))
+print("Ingresa la matriz: ")
+print("Ejemplo: ")
+print("2 5 18 8")
+print("5 18 46")
+print("18 46 327")
 for i in range(rows):
     tablaA.append(list(map(int, input().rstrip().split())))
 

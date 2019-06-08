884445e0203f4f5dbfb643dedc0b138cd8fd6104
jalzat39 <jmiguelalzate@gmail.com>
2019-06-07 21:21:28 -0500

    pivoteo escalonado terminado

--- Solo/pivoteoEscalonado.py
+++ Solo/pivoteoEscalonado.py
@@ -1,2 +1,14 @@
-def pivoteoEscalonado():
-    
+def pivoteo_escalonado(Ab,k, n, s):
+    mayor = 0
+    fila_mayor = k
+    cocientes = []
+    for i in range(k,n):
+        cocientes.append(abs(Ab[i][k])/s[i])
+    fila_mayor = max(range(len(cocientes)), key = lambda i: cocientes[i])
+    mayor = cocientes[fila_mayor]
+    if mayor == 0:
+        print "El sistema no tiene solucion unica"
+    elif fila_mayor != k:
+        Ab[k], Ab[fila_mayor] = Ab[fila_mayor], Ab[k]
+        s[k],s[fila_mayor] = s[fila_mayor],s[k]
+    return Ab

1af6e1d60b6e5f6874c454bd188a3a023c8ea14f
jalzat39 <jmiguelalzate@gmail.com>
2019-06-07 21:22:23 -0500

    pivoteo escalonado corregido

--- Solo/pivoteoEscalonado.py
+++ Solo/pivoteoEscalonado.py
@@ -7,7 +7,7 @@ def pivoteo_escalonado(Ab,k, n, s):
     fila_mayor = max(range(len(cocientes)), key = lambda i: cocientes[i])
     mayor = cocientes[fila_mayor]
     if mayor == 0:
-        print "El sistema no tiene solucion unica"
+        print("El sistema no tiene solucion unica")
     elif fila_mayor != k:
         Ab[k], Ab[fila_mayor] = Ab[fila_mayor], Ab[k]
         s[k],s[fila_mayor] = s[fila_mayor],s[k]
